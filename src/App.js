50f2dd1e944cef454405ca3a814bda6e5fba05b7
leonelborgesdev <leonel03t@gmail.com>
2024-04-07 14:33:47 -0400

    app.module.scss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import "./App.css";
+import Header from "./components/Header/Header"; // Importar como default export
+
+import css from "./styles/app.module.scss";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className={`bg-primary ${css.container}`}>
+      <Header />
+    </div>
+  );
 }
 
-export default App;
+export default App; // Asegúrate de exportar App correctamente
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Header = () => {
+  // Asegúrate de exportar el componente como default
+  return <div>Header</div>;
+};
+
+export default Header;
