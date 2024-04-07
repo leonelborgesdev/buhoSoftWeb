7c3f51a9bafc21a64e01e4a18bdef13f216a78fa
leonelborgesdev <leonel03t@gmail.com>
2024-04-07 17:41:44 -0400

    styles header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,36 @@
 import React from "react";
+import css from "./Header.module.scss";
+import { motion } from "framer-motion";
+import { headerVariants } from "../../utils/motion";
 
 const Header = () => {
   // Asegúrate de exportar el componente como default
-  return <div>Header</div>;
+  return (
+    <motion.div
+      initial="hidden"
+      whileInView="show"
+      variants={headerVariants}
+      className={`paddings ${css.wrapper}`}
+    >
+      <div className={`flexCenter innerWidth ${css.container}`}>
+        <div className={css.name}>Logo</div>
+        <ul className={`flexCenter ${css.menu}`}>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Services</a>
+          </li>
+          <li>
+            <a href="">Projects</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </motion.div>
+  );
 };
 
 export default Header;
