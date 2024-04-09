cff9bc373c74fce454d03005642d42374bf3b193
leonelborgesdev <leonel03t@gmail.com>
2024-04-09 10:24:12 -0400

    whoweare

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Experties from "./components/Experties/Experties";
 import Header from "./components/Header/Header";
 import { LandingHeader } from "./components/LandingHeader/LandingHeader";
+import WhoWeAre from "./components/WhoWeAre/WhoWeAre";
 
 import css from "./styles/app.module.scss";
 
@@ -9,6 +10,7 @@ function App() {
     <div className={`bg-primary ${css.container}`}>
       <Header />
       <LandingHeader />
+      <WhoWeAre />
       <Experties />
     </div>
   );
--- src/components/Experties/Experties.jsx
+++ src/components/Experties/Experties.jsx
@@ -6,7 +6,6 @@ import { fadeIn, staggerContainer, textVariant } from "../../utils/motion.js";
 const Experties = () => {
   return (
     <section className={css.wrapper}>
-      <a className="anchor" id="experties"></a>
       <motion.div
         variants={staggerContainer}
         initial="hidden"
