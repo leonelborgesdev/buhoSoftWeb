043f21448c674fbf59e90a3ad650b7397c9fd8fc
leonelborgesdev <leonel03t@gmail.com>
2024-04-09 12:33:36 -0400

    leftSide

--- src/components/WhoWeAre/WhoWeAre.jsx
+++ src/components/WhoWeAre/WhoWeAre.jsx
@@ -1,23 +1,28 @@
 import React from "react";
 import teamProgramers from "../../assets/teamProgramers.jpg";
 import { WhoWeAreParagra } from "../../utils/data";
+import css from "./WhoWeAre.module.scss";
 
 const WhoWeAre = () => {
   return (
-    <div>
-      <div>
-        <img src={teamProgramers} alt="" />
+    <section className={css.wrapper}>
+      <div
+        className={`paddings yPaddings innerWidth flexCenter ${css.container}`}
+      >
+        <div className={css.leftSide}>
+          <img src={teamProgramers} alt="" />
+        </div>
+        <div className={css.rightSide}>
+          <span className="secondaryText">Quienes Somos</span>
+          <span className="primaryText">Somos Desarrolladores Web</span>
+          {WhoWeAreParagra.map((paragraph, i) => {
+            <span className="secondaryText" key={i}>
+              {paragraph}
+            </span>;
+          })}
+        </div>
       </div>
-      <div>
-        <span className="secondaryText">Quienes Somos</span>
-        <span className="primaryText">Somos Desarrolladores Web</span>
-        {WhoWeAreParagra.map((paragraph, i) => {
-          <span className="secondaryText" key={i}>
-            {paragraph}
-          </span>;
-        })}
-      </div>
-    </div>
+    </section>
   );
 };
 
