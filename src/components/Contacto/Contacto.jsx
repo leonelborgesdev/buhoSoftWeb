760251a47b1780260c308c4ffa2439823e7694e6
leonelborgesdev <leonel03t@gmail.com>
2024-04-25 12:13:52 -0400

    contacto

---
+++ src/components/Contacto/Contacto.jsx
@@ -0,0 +1,50 @@
+import React, { useRef } from "react";
+import { useSelector } from "react-redux";
+import emailjs from "emailjs-com";
+import "./Contacto.css";
+
+export const Contacto = () => {
+  const form = useRef();
+  const handleEnviarEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "gmailMessage",
+        "template_6n5qu8p",
+        form.current,
+        "lna8Tsiq7_5RTw76n"
+      )
+      .then(
+        (result) => {
+          alert("mensage enviado correctamente");
+        },
+        (error) => {
+          alert(error);
+        }
+      );
+    e.target.reset();
+  };
+  return (
+    <form ref={form} onSubmit={handleEnviarEmail}>
+      <div className="container_all_contact">
+        <div className="container_body_contact">
+          <div className="inputBox">
+            <input type="text" required="required" name="name" />
+            <span>{"Name"}</span>
+          </div>
+          <div className="inputBox">
+            <input type="text" required="required" name="email" />
+            <span>Email</span>
+          </div>
+          <div className="inputBox">
+            <textarea name="message" />
+            <span>{"Messegge"}</span>
+          </div>
+          <div className="buton_submit_send">
+            <input type="submit" value={"Send"} />
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+};
