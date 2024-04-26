1d8e982827ab5775cc57a4d3aa55ea0556f5b3bd
leonelborgesdev <leonel03t@gmail.com>
2024-04-26 11:23:36 -0400

    RedesSociales.module.scss

--- src/components/RedesSociales/RedesSociales.jsx
+++ src/components/RedesSociales/RedesSociales.jsx
@@ -2,10 +2,11 @@ import { FaWhatsappSquare } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
+import css from "./RedesSociales.module.scss";
 
 export const RedesSociales = () => {
   return (
-    <div>
+    <div className={css.containerredessociales}>
       <FaTwitter size={25} color="black" />
       <FaFacebook size={25} color="black" />
       <FaWhatsappSquare size={25} color="black" />
