d1c9f88099b6bf230d5c5eb7dcd15033e80021ff
leonelborgesdev <leonel03t@gmail.com>
2024-05-07 12:42:39 -0400

    instagram whatsapp facebook

--- src/components/RedesSociales/RedesSociales.jsx
+++ src/components/RedesSociales/RedesSociales.jsx
@@ -7,10 +7,16 @@ import css from "./RedesSociales.module.scss";
 export const RedesSociales = () => {
   return (
     <div className={css.containerredessociales}>
+      <a href="https://www.instagram.com/" target={"_blank"} rel="noreferrer">
+        <PiInstagramLogoFill size={35} color="black" />
+      </a>
+      <a href="https://wa.me/+59175124608" target={"_blank"} rel="noreferrer">
+        <IoLogoWhatsapp size={35} color="black" />
+      </a>
+      <a href="https://www.facebook.com/" target={"_blank"} rel="noreferrer">
+        <FaFacebook size={35} color="black" />
+      </a>
       <FaTwitter size={35} color="black" />
-      <FaFacebook size={35} color="black" />
-      <IoLogoWhatsapp size={35} color="black" />
-      <PiInstagramLogoFill size={35} color="black" />
     </div>
   );
 };
